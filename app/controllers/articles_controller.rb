65d50b09056ddac42a3e63a91d90fbc3053bf9af
vladimirlozitskiy23 <rubyonrails2023@gmail.com>
2023-05-21 18:54:08 +0300

    Add GRUD for article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,50 @@
 class ArticlesController < ApplicationController
     def index
-        
+        @articles = Article.all 
+    end
+
+    def show
+        @article = Article.find(params[:id])
+    end
+
+    def new
+        @article = Article.new
+    end
+
+    def create
+        @article = Article.new(article_params)
+        if @article.save
+          redirect_to @article
+        else
+            render action: 'new'
+        end
+    end
+
+    def edit
+        @article = Article.find(params[:id])
+    end
+
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(article_params)
+            redirect_to @article
+        else
+            render action: 'edit'
+
+        end
+    end
+
+
+    def destroy
+        @article = Article.find(params[:id])
+        @article.destroy
+        redirect_to @article
+
+    end
+
+    private
+
+    def article_params
+        params.require(:article).permit(:title, :body)
     end
 end
